6d8d89741d1a4ace0aca42c663948389d01d7ef0
Alexander <thesolve@mail.ru>
2019-03-15 22:27:51 +0300

    Добавил компиляцию файлов solidity. Реализуется проверка ошибок. Для этого взял некоторый код из solcjs.

--- app.js
+++ app.js
@@ -1,3 +1,79 @@
 'use strict';
 
-console.log('Hello world');
+const abi = require('ethereumjs-abi');
+const solc = require('solc');
+const cmd = require('minimist')(process.argv.slice(2));
+const fs = require('fs');
+
+const files = cmd._;
+const contract = cmd.c;
+const types = cmd.t;
+const values = cmd.v;
+const output = cmd.o;
+
+console.log(files);
+console.log(cmd);
+
+if (files === undefined || contract === undefined)
+{
+    abort('Вы должны указать имя файла для компиляции и имя контракта.');
+}
+
+var sources = {};
+
+for (var i = 0; i < files.length; i++)
+{
+    try
+    {
+        sources[files[i]] = { content: fs.readFileSync(files[i]).toString() };
+    }
+    catch (e)
+    {
+        abort('Error reading ' + files[i] + ': ' + e);
+    }
+}
+
+const params =
+{
+    language: "Solidity",
+    sources: sources,
+    settings:
+    {
+        outputSelection:
+        {
+            "*":
+            {
+                "*": ["abi", "evm.bytecode"]
+            }
+        }
+    }
+};
+
+const compiled = JSON.parse(solc.compileStandardWrapper(JSON.stringify(params)));
+if (!compiled)
+{
+    abort('No output from compiler');
+}
+else if (output['errors'])
+{
+    for (var error in output['errors'])
+    {
+        var message = output['errors'][error];
+        if (message.severity === 'warning')
+        {
+            console.log(message.formattedMessage);
+        }
+        else
+        {
+            console.error(message.formattedMessage);
+        }
+    }
+}
+
+function abort(msg)
+{
+    console.error(msg || 'Error occured');
+    process.exit(1);
+}
+
+//console.log('Hello world');
